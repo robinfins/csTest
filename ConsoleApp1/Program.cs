217c00fc042094afd0b19756d7de93cc2363e2ea
Robin Finsådal <robinfinsadal@gmail.com>
2024-08-17 10:16:05 +0200

    v1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// hello world program
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");    
+        }
+    }
+}
